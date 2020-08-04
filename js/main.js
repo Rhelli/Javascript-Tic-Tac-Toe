fecff35672049b64597a270be0cda915dd742c21
Alexis <psialedev@gmail.com>
2020-08-04 17:05:34 -0500

    created displayController module and developed an addEvents function

--- js/main.js
+++ js/main.js
@@ -2,28 +2,43 @@
 const Player = (name, symbol) => {
   chooseName = () => name;
   chooseSymbol = () => symbol;
+  const addSymbol = (item,symbol) => { item.innerHTML = symbol }
   const sayName = function() {
     console.log(`Yo waddup, ma name is ${name}.`);
   };
   const saySymbol = function() {
     console.log(`Yo was happenin sun, ma gang symbols be ${symbol}.`);
   };
-  return { sayName, saySymbol };
+  return { sayName, saySymbol, addSymbol };
 };
 
 
 // GAMEBOARD - MODULE / IIFE
-//const gameBoard = (function() {
-//  let board = `
-//  +-----+-----+-----+
-//  |  ${}  |  ${}  |  ${}  |
-//  +-----+-----+-----+
-//  |  ${}  |  ${}  |  ${}  |
-//  +-----+-----+-----+
-//  |  ${}  |  ${}  |  ${}  |
-//  +-----+-----+-----+
-//  `
-//})();
+const gameBoard = (function() {
+  let board = [
+    ['O','X','O'],
+    ['X','O','O'],
+    ['X','O','X']]
+    const getBoard = () => board
+    return { getBoard }
+})();
+
+const displayController = (function(doc, player) {
+  const boardContainer = doc.getElementById("gameboard");
+  const childrens = boardContainer.childNodes;
+  
+  const addEvents = childrens.forEach(element => {
+    element.addEventListener('click',() => {
+      player.addSymbol(element,symbol);
+    })
+  });
+  const renderBoard = () => { boardContainer.innerHTML = gameBoard.getBoard.map(element => {
+    ``
+  }) }
+  return { addEvents }
+})(document);
+
+displayController.addEvents;
 
 // DISPLAY CONTROLLER - MODULE
 
