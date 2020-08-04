bb0830342729031e50efcda7a43ed34371102464
Rory Hellier <rory.hellier@googlemail.com>
2020-08-04 23:39:06 +0100

    Added Function Suggestions to Structure the Project

--- js/main.js
+++ js/main.js
@@ -2,44 +2,53 @@
 const Player = (name, symbol) => {
   chooseName = () => name;
   chooseSymbol = () => symbol;
-  const addSymbol = (item,symbol) => { item.innerHTML = symbol }
-  const sayName = function() {
+  const addSymbol = (item, symbol) => { item.innerHTML = symbol; };
+  const sayName = function () {
     console.log(`Yo waddup, ma name is ${name}.`);
   };
-  const saySymbol = function() {
+  const saySymbol = function () {
     console.log(`Yo was happenin sun, ma gang symbols be ${symbol}.`);
   };
   return { sayName, saySymbol, addSymbol };
 };
 
 
-// GAMEBOARD - MODULE / IIFE
-const gameBoard = (function() {
-  let board = [
-    ['O','X','O'],
-    ['X','O','O'],
-    ['X','O','X']]
-    const getBoard = () => board
-    return { getBoard }
-})();
+// GAMEBOARD / GAMESTATE
+const gameBoard = (function () {
+  const board = [
+    ['O', 'X', 'O'],
+    ['X', 'O', 'O'],
+    ['X', 'O', 'X']];
+  const getBoard = () => board;
+  return { getBoard };
+}());
 
-const displayController = (function(doc, player) {
-  const boardContainer = doc.getElementById("gameboard");
+// VISUAL / RENDER BOARD
+const displayController = (function (doc, player) {
+  const boardContainer = doc.getElementById('gameboard');
   const childrens = boardContainer.childNodes;
-  
+
   const addEvents = childrens.forEach(element => {
-    element.addEventListener('click',() => {
-      player.addSymbol(element,symbol);
-    })
+    element.addEventListener('click', () => {
+      player.addSymbol(element, symbol);
+    });
   });
   // const renderBoard = () => { boardContainer.innerHTML = gameBoard.getBoard.map(element => {
   //   ``
   // }) }
-  return { addEvents }
-})(document);
+  return { addEvents };
+}(document));
 
 displayController.addEvents;
 
-// DISPLAY CONTROLLER - MODULE
+// USER MESSAGES
 
-// GAME LOGIC - SUPER SRS SECTION
+// FUNCTION - HANDLE THE CELL PLAYED
+
+// FUNCTION - HANDLE THE PLAYER CHANGE
+
+// FUNCTION HANDLE THE RESULT/MOVE VALIDATION - IS THE GAME WON, DRAWN OR LOST?
+
+// FUNCTION - HANDLE CELL CLICK
+
+// FUNCTION - HANDLE GAME RESTART AFTER FINISH
