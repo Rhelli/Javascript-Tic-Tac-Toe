24792503b8e77bf83dafc7c9bb31bf5576a04284
psialedev <psialedev@gmail.com>
2020-08-05 16:35:20 -0500

    make a super module

--- js/main.js
+++ js/main.js
@@ -1,115 +1,126 @@
-// PLAYERS - FACTORY FUNCTIONS
-const Player = (name, symbol) => {
-  const namePlayerOne = document.getElementById('player-one').value;
-  const namePlayerTwo = document.getElementById('player-two').value;
-  getName = () =>  name;
-  setNamePlayerOne = () => namePlayerOne;
-  setNamePlayerTwo = () => namePlayerTwo;
-  const addSymbol = (cell, symbol) => { cell.innerHTML = symbol; };
-  return { getName };
-};
-
-
-// const createPlayerOne = (() => {
-//   const playerDOM = document.getElementById('player-one');
-//   const name = playerDOM.value;
-//   const player = Player(name,'X');
-// })();
-
-const createPlayers = ((array) => {
-  const newPlayerArray = [];
-  const playerOne = array[0];
-  const playerTwo = array[1];
-  newPlayerArray.push(Player(playerOne, 'X'));
-  newPlayerArray.push(Player(playerTwo, 'O'));
-  const getPlayerArray = () => {
-    return newPlayerArray;
+const game = (() =>{ // PLAYERS - FACTORY FUNCTIONS
+  const namePlayerOne = document.querySelector('#player-one').value;
+  const namePlayerTwo = document.querySelector('#player-two').value;
+  let currentPlayer = '';
+  const Player = (name, symbol) => {
+    getName = () =>  name;
+    const addSymbol = (cell, symbol) => { cell.innerHTML = symbol; };
+    return { name, symbol };
   };
-  return { getPlayerArray };
+
+
+  const gameInit = () => {
+    if (namePlayerOne.value !== '' && namePlayerTwo.value !== '') {
+      playerOne = Player(namePlayerOne.value, 'X');
+      playerTwo = Player(namePlayerTwo.value, 'O');
+      currentPlayer = playerOne;
+      console.log(`playerOne: ${playerOne.name} playerTwo: ${playerTwo}`)
+    }
+  };
+
+  // const createPlayerOne = (() => {
+  //   const playerDOM = document.getElementById('player-one');
+  //   const name = playerDOM.value;
+  //   const player = Player(name,'X');
+  // })();
+
+  const CreatePlayers = (() => {
+    const newPlayerArray = [];
+    const setPlayersArray = (array) => {
+    const playerOne = array[0];
+    const playerTwo = array[1];
+    newPlayerArray.push(Player(playerOne, 'X'));
+    newPlayerArray.push(Player(playerTwo, 'O'));
+    return newPlayerArray;
+    }
+    const getPlayerArray = (array) =>  setPlayersArray(array);
+    return { getPlayerArray, setPlayersArray };
+  })();
+  return { gameInit }
 })();
 
-const playersEmpty = () => {
-  const playersArray = [];
-  if (document.getElementById('player-one').value.length === 0 && document.getElementById('player-two').value.length === 0) {
-    alert('Please Complete All Fields!');
-    throw new Error('Player name is empty!');
-  } else {
-    const playerOne = document.getElementById('player-one').value;
-    const playerTwo = document.getElementById('player-two').value;
-    playersArray.push(playerOne);
-    playersArray.push(playerTwo);
-  }
-  return { playersArray };
-};
+game.gameInit()
 
-createPlayers(playersArray).getPlayersArray;
+  // const playersEmpty = () => {
+  //   const playersArray = [];
+  //   if (document.getElementById('player-one').value.length === 0 && document.getElementById('player-two').value.length === 0) {
+  //     alert('Please Complete All Fields!');
+  //     throw new Error('Player name is empty!');
+  //   } else {
+  //     const playerOne = document.getElementById('player-one').value;
+  //     const playerTwo = document.getElementById('player-two').value;
+  //     playersArray.push(playerOne);
+  //     playersArray.push(playerTwo);
+  //   }
+  //   return playersArray;
+  // };
 
-//const createPlayer = (() => {
-//  const players = [];
-//  if (players.length === 0) {
-//    let TheOriginalPlayer = Player(name, 'X');
-//    players.push(TheOriginalPlayer);
-//  } else {
-//    let luigiOrLoserPlayer = Player(name, 'O');
-//    players.push(luigiOrLoserPlayer);
-//  }
-//  return { players };
-//})();
+  //const createPlayer = (() => {
+  //  const players = [];
+  //  if (players.length === 0) {
+  //    let TheOriginalPlayer = Player(name, 'X');
+  //    players.push(TheOriginalPlayer);
+  //  } else {
+  //    let luigiOrLoserPlayer = Player(name, 'O');
+  //    players.push(luigiOrLoserPlayer);
+  //  }
+  //  return { players };
+  //})();
 
-// GAMEBOARD / GAMESTATE
-const gameBoard = (function () {
-  const board = [
-    ['O', 'X', 'O'],
-    ['X', 'O', 'O'],
-    ['X', 'O', 'X']];
-  const getBoard = () => board;
-  return { getBoard };
-}());
+  // GAMEBOARD / GAMESTATE
+  const gameBoard = (function () {
+    const board = [
+      ['O', 'X', 'O'],
+      ['X', 'O', 'O'],
+      ['X', 'O', 'X']];
+    const getBoard = () => board;
+    return { getBoard };
+  }());
 
-// VISUAL / RENDER BOARD
-// const displayController = (function (doc, player) {
-//   const boardContainer = doc.getElementById('gameboard');
-//   const childrens = boardContainer.childNodes;
+  // VISUAL / RENDER BOARD
+  // const displayController = (function (doc, player) {
+  //   const boardContainer = doc.getElementById('gameboard');
+  //   const childrens = boardContainer.childNodes;
 
-//   const addEvents = childrens.forEach(element => {
-//     element.addEventListener('click', () => {
-//       player.addSymbol(element, symbol);
-//     });
-//   });
-//   // const renderBoard = () => { boardContainer.innerHTML = gameBoard.getBoard.map(element => {
-//   //   ``
-//   // }) }
-//   return { addEvents };
-// }(document));
+  //   const addEvents = childrens.forEach(element => {
+  //     element.addEventListener('click', () => {
+  //       player.addSymbol(element, symbol);
+  //     });
+  //   });
+  //   // const renderBoard = () => { boardContainer.innerHTML = gameBoard.getBoard.map(element => {
+  //   //   ``
+  //   // }) }
+  //   return { addEvents };
+  // }(document));
 
-// displayController.addEvents;
-
-
-
-// USER MESSAGES
-// const userMessage = ((playerOne, playerTwo, winner, loser) => {
-//   let name = '';
-//   const playerOneAssign = `Congratulations, ${playerOne}, you are player one.`;
-//   const playerTwoAssign = `${playerTwo}, you are player two. Unlucky.`;
-//   const gameWelcome = `${playerOne}, ${playerTwo}, we're so sorry that you've had the misfortune of playing this terribly made game of tic-tac-toe. But you're here now, so we may as well make the most of it.`;
-//   const rulesPrompt = 'Would you like to know the rules of the game?';
-//   const rulesStatement = "The aim of the game is to place three of your symbols in a row before the other player. You can place your symbols horizontally, vertically or diagonally, however, they must be in a continuous, unbroken line. Each player gets one move before the other player gets theirs. The first player to get three in a row wins. LET'S PLAY MURDERBALL!!!!!";
-//   const gameStart = 'LETS PLAY TIC TAC TOE!!!!!!!!';
-//   const playerOneMovePrompt = `${playerOne}, make your move.`;
-//   const playerTwoMovePrompt = `${playerTwo}, you're not as good or as beautiful as ${playerOne}, so just take your turn so we can stop looking at you already.`;
-//   const gameDrawMessage = "Wow, you're both as bad as each other. Well done.";
-//   const gameWinMessage = `${winner}, congratulations, you are a weiner. ${loser}, better luck next time.`;
-//   const rematchPrompt = `${loser}, don't let ${winner} get away with it. Even if he is the weiner. Play again?`;
-// })(playerOne, playerTwo, winner, loser);
+  // displayController.addEvents;
 
 
-// FUNCTION - HANDLE THE CELL PLAYED
 
-// FUNCTION - HANDLE THE PLAYER CHANGE
+  // USER MESSAGES
+  // const userMessage = ((playerOne, playerTwo, winner, loser) => {
+  //   let name = '';
+  //   const playerOneAssign = `Congratulations, ${playerOne}, you are player one.`;
+  //   const playerTwoAssign = `${playerTwo}, you are player two. Unlucky.`;
+  //   const gameWelcome = `${playerOne}, ${playerTwo}, we're so sorry that you've had the misfortune of playing this terribly made game of tic-tac-toe. But you're here now, so we may as well make the most of it.`;
+  //   const rulesPrompt = 'Would you like to know the rules of the game?';
+  //   const rulesStatement = "The aim of the game is to place three of your symbols in a row before the other player. You can place your symbols horizontally, vertically or diagonally, however, they must be in a continuous, unbroken line. Each player gets one move before the other player gets theirs. The first player to get three in a row wins. LET'S PLAY MURDERBALL!!!!!";
+  //   const gameStart = 'LETS PLAY TIC TAC TOE!!!!!!!!';
+  //   const playerOneMovePrompt = `${playerOne}, make your move.`;
+  //   const playerTwoMovePrompt = `${playerTwo}, you're not as good or as beautiful as ${playerOne}, so just take your turn so we can stop looking at you already.`;
+  //   const gameDrawMessage = "Wow, you're both as bad as each other. Well done.";
+  //   const gameWinMessage = `${winner}, congratulations, you are a weiner. ${loser}, better luck next time.`;
+  //   const rematchPrompt = `${loser}, don't let ${winner} get away with it. Even if he is the weiner. Play again?`;
+  // })(playerOne, playerTwo, winner, loser);
 
 
-// FUNCTION HANDLE THE RESULT/MOVE VALIDATION - IS THE GAME WON, DRAWN OR LOST?
+  // FUNCTION - HANDLE THE CELL PLAYED
 
-// FUNCTION - HANDLE CELL CLICK
+  // FUNCTION - HANDLE THE PLAYER CHANGE
 
-// FUNCTION - HANDLE GAME RESTART AFTER FINISH
+
+  // FUNCTION HANDLE THE RESULT/MOVE VALIDATION - IS THE GAME WON, DRAWN OR LOST?
+
+  // FUNCTION - HANDLE CELL CLICK
+
+  // FUNCTION - HANDLE GAME RESTART AFTER FINISH
