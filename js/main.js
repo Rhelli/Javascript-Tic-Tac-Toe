1f2a711877649dea2aac5966d15b9bd78d8fee0e
Rory Hellier <rory.hellier@googlemail.com>
2020-08-05 17:17:04 +0100

    Completed A Basic Draft Of The User Messages Module
     - Added player name prompts
     - Added player assign messages
     - Added game welcome, rules and all other question prompts
     - Added end game messages and rematch questions

--- js/main.js
+++ js/main.js
@@ -3,12 +3,6 @@ const Player = (name, symbol) => {
   chooseName = () => name;
   chooseSymbol = () => symbol;
   const addSymbol = (item, symbol) => { item.innerHTML = symbol; };
-  const sayName = function () {
-    console.log(`Yo waddup, ma name is ${name}.`);
-  };
-  const saySymbol = function () {
-    console.log(`Yo was happenin sun, ma gang symbols be ${symbol}.`);
-  };
   return { sayName, saySymbol, addSymbol };
 };
 
@@ -42,6 +36,22 @@ const displayController = (function (doc, player) {
 displayController.addEvents;
 
 // USER MESSAGES
+const userMessage = ((playerOne, playerTwo, winner, loser) => {
+  const playerOneNamePrompt = 'Player one, please enter your name...';
+  const playerTwoNamePrompt = 'Player two, please enter your name...';
+  const playerOneAssign = `Congratulations, ${playerOne}, you are player one.`;
+  const playerTwoAssign = `${playerTwo}, you are player two. Unlucky.`;
+  const gameWelcome = `${playerOne}, ${playerTwo}, we're so sorry that you've had the misfortune of playing this terribly made game of tic-tac-toe. But you're here now, so we may as well make the most of it.`;
+  const rulesPrompt = 'Would you like to know the rules of the game?';
+  const rulesStatement = "The aim of the game is to place three of your symbols in a row before the other player. You can place your symbols horizontally, vertically or diagonally, however, they must be in a continuous, unbroken line. Each player gets one move before the other player gets theirs. The first player to get three in a row wins. LET'S PLAY MURDERBALL!!!!!";
+  const gameStart = 'LETS PLAY TIC TAC TOE!!!!!!!!';
+  const playerOneMovePrompt = `${playerOne}, make your move.`;
+  const playerTwoMovePrompt = `${playerTwo}, you're not as good or as beautiful as ${playerOne}, so just take your turn so we can stop looking at you already.`;
+  const gameDrawMessage = "Wow, you're both as bad as each other. Well done.";
+  const gameWinMessage = `${winner}, congratulations, you are a weiner. ${loser}, better luck next time.`;
+  const rematchPrompt = `${loser}, don't let ${winner} get away with it. Even if he is the weiner. Play again?`;
+})(playerOne, playerTwo, winner, loser);
+
 
 // FUNCTION - HANDLE THE CELL PLAYED
 
