8e66809683c47e9cd2d7f2700c816d0c21b5a6a5
Rory Hellier <rory.hellier@googlemail.com>
2020-08-10 22:24:51 +0100

    Implemented winning cell highlight after new winningCondition function was put in. Broke other things....

--- js/main.js
+++ js/main.js
@@ -6,50 +6,50 @@ const game = (() => {
   const namePlayerTwo = document.getElementById('player-two');
   const allIcons = document.querySelectorAll('.character-item');
   let currentPlayer = '';
-  let Icons = [];
+  const Icons = [];
 
   const Player = (name, symbol, playerNumber, img) => {
-    getName = () => name;
-    getSymbol = () => symbol;
-    setPlayerSymbol = (symbol) => img = symbol;
-    getImg  = () => img;
-    getNumber = () => playerNumber;
-    return { getName, getSymbol, getNumber, getImg, setPlayerSymbol };
+    const getName = () => name;
+    const getSymbol = () => symbol;
+    const setPlayerSymbol = (symbol) => {
+      img = symbol;
+    };
+    const getImg = () => img;
+    const getNumber = () => playerNumber;
+    return {
+      getName, getSymbol, getNumber, getImg, setPlayerSymbol,
+    };
   };
 
   const choosePlayerIcon = (event) => {
     const chosenIcon = event.target.src;
-    if(Icons.length < 2) { 
-    if(numberOfPlayer == 1) {
-      Icons.push(chosenIcon);
-      console.log(`player one ${[Icons[0]]}`)
-      numberOfPlayer = 2;
+    if (Icons.length < 2) {
+      if (numberOfPlayer = 1) {
+        Icons.push(chosenIcon);
+        numberOfPlayer = 2;
+      } else {
+        Icons.push(chosenIcon);
+        numberOfPlayer = 1;
+      }
     }
-    else {
-      Icons.push(chosenIcon);
-      console.log(`player two: ${Icons[1]}`)
-      numberOfPlayer = 1;
-    }
-  }
 
     // currentPlayer.setPlayerSymbol(chosenSymbol);
     return { Icons };
   };
-  
-  
+
+
   const gameInit = () => {
-    if (namePlayerOne.value !== '' && namePlayerTwo.value !== '' && Icons.length == 2) {
-      playerOne = Player(namePlayerOne.value, 'X',1,Icons[0]);
-      playerTwo = Player(namePlayerTwo.value, 'O',2,Icons[1]);
-      console.log(`player one: ${playerOne} player two: ${playerTwo}`)
+    if (namePlayerOne.value !== '' && namePlayerTwo.value !== '' && Icons.length === 2) {
+      playerOne = Player(namePlayerOne.value, 'X', 1, Icons[0]);
+      playerTwo = Player(namePlayerTwo.value, 'O', 2, Icons[1]);
       currentPlayer = playerOne;
     }
   };
 
-  allIcons.forEach(element => { element.addEventListener('click', choosePlayerIcon, false);});
+  allIcons.forEach(element => { element.addEventListener('click', choosePlayerIcon, false); });
 
   // GAMEBOARD
-  const gameBoard = (() => {  
+  const gameBoard = (() => {
     const board = ['', '', '', '', '', '', '', '', ''];
     const boardContainer = document.getElementById('gameboard');
     const cells = document.querySelectorAll('.cell');
@@ -79,40 +79,75 @@ const game = (() => {
       alert('LETS NOT PLAY MURDERBALL!!!!!!');
     };
 
-    const winningValidation = (array, symbol) => {
-      // HORIZONTAL
-      if (checkForThree(array[0], array[1], array[2], symbol)) {
-        const win = document.querySelectorAll("#c1, #c2, #c3");
-        let winningwin = [...win]
-        winningwin.forEach(element => {
-          element.style.background = "green ";
-        });
-        return true;
-        
+    //const winningValidation = (array, symbol) => {
+    //  // HORIZONTAL
+    //  if (checkForThree(array[0], array[1], array[2], symbol)) {
+    //    const win = document.querySelectorAll('#c1, #c2, #c3');
+    //    const winningwin = [...win];
+    //    winningwin.forEach(element => {
+    //      element.style.background = 'green ';
+    //    });
+    //    return true;
+    //  }
+    //  if (checkForThree(array[3], array[4], array[5], symbol)) {
+    //    return true;
+    //  }
+    //  if (checkForThree(array[6], array[7], array[8], symbol)) {
+    //    return true;
+    //  }
+    //  // VERTICAL
+    //  if (checkForThree(array[0], array[3], array[6], symbol)) {
+    //    return true;
+    //  }
+    //  if (checkForThree(array[1], array[4], array[7], symbol)) {
+    //    return true;
+    //  }
+    //  // DIAGONAL
+    //  if (checkForThree(array[2], array[5], array[8], symbol)) {
+    //    return true;
+    //  }
+    //  if (checkForThree(array[0], array[4], array[8], symbol)) {
+    //    return true;
+    //  }
+    //  if (checkForThree(array[2], array[4], array[6], symbol)) {
+    //    return true;
+    //  }
+    //};
+
+    const winningValidation = (array) => {
+      let roundWon = false;
+      const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
+      for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        const a = array[winCondition[0]];
+        const b = array[winCondition[1]];
+        const c = array[winCondition[2]];
+        const aa = document.getElementById(`c${winCondition[0]}`);
+        const bb = document.getElementById(`c${winCondition[1]}`);
+        const cc = document.getElementById(`c${winCondition[2]}`);
+        if (a === undefined || b === undefined || c === undefined) {
+          continue;
+        }
+        if (a === b && b === c) {
+          const style = [aa, bb, cc];
+          roundWon = true;
+          style.forEach(element => {
+            element.style.background = 'green ';
+          });
+          return roundWon;
+        }
       }
-      if (checkForThree(array[3], array[4], array[5], symbol)) {
-        return true;
-      }
-      if (checkForThree(array[6], array[7], array[8], symbol)) {
-        return true;
-      }
-      // VERTICAL
-      if (checkForThree(array[0], array[3], array[6], symbol)) {
-        return true;
-      }
-      if (checkForThree(array[1], array[4], array[7], symbol)) {
-        return true;
-      }
-      // DIAGONAL
-      if (checkForThree(array[2], array[5], array[8], symbol)) {
-        return true;
-      }
-      if (checkForThree(array[0], array[4], array[8], symbol)) {
-        return true;
-      }
-      if (checkForThree(array[2], array[4], array[6], symbol)) {
-        return true;
-      }
+      return roundWon;
     };
 
     const updateBoardArray = () => {
@@ -122,23 +157,62 @@ const game = (() => {
     };
 
     const playerSwitch = () => {
-      console.log(count);
-      console.log(updateBoardArray());
-      if (winningValidation(updateBoardArray(), currentPlayer.getSymbol()) || count === 9) {
-        setTimeout(() => {
-          const rematch = confirm(`${currentPlayer.getName()} has won. Would you like to play again?`);
-          if (rematch === true) {
-            reset(cells);
-          } else {
-            alert('Loser.');
-          };
-        }, 450);
-      } else if (currentPlayer.getNumber() === 1) {
-        currentPlayer = playerTwo;
-      } else {
-        currentPlayer = playerOne;
-      }
+      const rematch = confirm(`${currentPlayer.getName()} has won. Would you like to play again?`);
+      const drawRematch = confirm(`Wow ${playerOne}, how did you not manage to beat ${playerTwo}? That guy is an IDIOT.`);
+      let winCheck = winningValidation(updateBoardArray());
+      //if (winningValidation(updateBoardArray())) {
+      //  setTimeout(() => {
+      //    if (rematch === true) {
+      //      reset(cells);
+      //    } else {
+      //      alert('Loser.');
+      //    }
+      //  }, 450);
+      //} else if (currentPlayer.getNumber() === 1) {
+      //  currentPlayer = playerTwo;
+      //} else {
+      //  currentPlayer = playerOne;
+      //}
+      //if (winningValidation(updateBoardArray() === false && count === 9)) {
+      //  setTimeout(() => {
+      //    if (drawRematch === true) {
+      //      reset(cells);
+      //    } else {
+      //      alert(`${playerOne}, I had so much faith in you....`);
+      //    }
+      //  }, 450);
+      //}
+      switch(winCheck) {
+        case true:
+          setTimeout(() => {
+            if (rematch === true) {
+              reset(cells);
+            } else {
+              alert('Loser.');
+            }
+          }, 450);
+          break;
+
+        case false && count === 9:
+          setTimeout(() => {
+            if (drawRematch === true) {
+              reset(cells);
+            } else {
+              alert(`${playerOne}, I had so much faith in you....`);
+            }
+          }, 450);
+          break;
+
+        //default:
+        //  (function() { 
+        //    if (currentPlayer.getNumber() === 1) {
+        //    currentPlayer = playerTwo;
+        //    } else {
+        //    currentPlayer = playerOne;
+        //    }
+        //  })();
       count++;
+      }
     };
 
     const ifCellEmpty = (event, symbol, img) => {
@@ -167,7 +241,6 @@ const game = (() => {
 
     cells.forEach(cell => cell.addEventListener('click', clickCell, false));
     return { clickCell, reset, resetButton };
-
   })();
 
 
@@ -194,45 +267,4 @@ TO DO LIST
     - RED & GREEN TRAFFIC LIGHTS BESIDE EACH PLAYERS ICON, GREEN FOR THEIR TURN AND RED WHEN IT ISNT
 
 */
-const winningValidation = (array, symbol) => {
-  const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  for (let i = 0; i <= 7; i++) {
-    const winCondition = winningConditions[i];
-    let a = gameState[ winCondition[0] ];
-    let b = gameState[ winCondition[1] ];
-    let c = gameState[ winCondition[2] ];
-    let aa = document.getElementById(`c${winCondition[0]}`)
-    let bb = document.getElementById(`c${winCondition[1]}`)
-    let cc = document.getElementById(`c${winCondition[2]}`)
-    if (a === '' || b === '' || c === '') {
-        continue;
-    }
-    if (a === `${symb}` && b === `${symb}` && c === `${symb}`) {
-        let style = [...aa,...bb,...cc]
-        roundWon = true;
-        style.forEach(element => {
-          element.style.background = "green ";
-        });
-        break
-    }
-  }
-  
-  if(roundWon) {
-  
-  }
-}
-
-
-
-
 
