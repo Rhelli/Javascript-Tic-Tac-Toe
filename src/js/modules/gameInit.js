8cfcc428e096f923a5f045be114bf09fad03a7cd
Alexis <psialedev@gmail.com>
2020-11-03 09:58:01 -0600

    trying to make gameInit into several functions so we can pass current and opposite player state to other modules

--- src/js/modules/gameInit.js
+++ src/js/modules/gameInit.js
@@ -9,6 +9,39 @@ let playerTwo;
 let currentPlayer;
 let oppositePlayer;
 
+// if players dont have a name
+// if icons havent been selected
+// then 27 till 35
+// we could return an Array with two player objects
+//  and export that into clickCell 
+
+// const renderPlayer = () => {
+//   playerOne = player(domElements.namePlayerOne.value, 'X', 1, Icons[0], 'rgba(144, 164, 174, 0.8');
+//   playerTwo = player(domElements.namePlayerTwo.value, 'O', 2, Icons[1], 'rgba(241, 196, 15, 0.8');
+//   currentPlayer = playerOne;
+//   oppositePlayer = playerTwo;
+
+//   // return [
+//   //   playerOne, playerTwo,
+//   //   currentPlayer, oppositePlayer,
+//   // ];
+
+//   return true
+
+// };
+
+// const isGameReady = () => {
+//   if (domElements.namePlayerOne.value === '' || domElements.namePlayerTwo.value === '') {
+//     alert("Please add both players' names to continue.");
+//     return false;
+//   }
+//   if (Icons.length < 2) {
+//     alert('Please select a character for both players to continue.');
+//     return false;
+//   }
+//   return true;
+// };
+
 const gameInit = () => {
   console.log('sup perro');
   if (domElements.namePlayerOne.value === '' || domElements.namePlayerTwo.value === '') {
