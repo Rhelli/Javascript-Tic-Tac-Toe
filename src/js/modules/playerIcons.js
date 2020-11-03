d979fc514a4e7605b2769695105ee27877c13ff5
Alexis <psialedev@gmail.com>
2020-11-03 15:54:54 -0600

    refactored reset to just delete everything, and yes, it works.

--- src/js/modules/playerIcons.js
+++ src/js/modules/playerIcons.js
@@ -1,11 +1,15 @@
 import * as domElements from './domElements';
 import { retrieveItem, saveItem } from './localStorage';
 
+
+
+
 let numberOfPlayer = 1;
 const Icons = [];
 
 const choosePlayerIcon = (element) => {
   const chosenIcon = element;
+
   if (numberOfPlayer === 1) {
     if (Icons[0]) {
       document.getElementById('avoid-clicks').id = '';
--- src/js/modules/reset.js
+++ src/js/modules/reset.js
@@ -7,33 +7,9 @@ import { retrieveItem, saveItem } from './localStorage';
 
 
 const reset = () => {
-  const currentPlayer = retrieveItem('currentPlayer');
-  let count = retrieveItem('count')
-  domElements.cells.forEach((element) => {
-    element.addEventListener('click', clickCell, false);
-    element.innerHTML = '';
-    element.dataset.datasymbol = '';
-    domManipulation.initialBackground(element);
-    count = 1;
-    domElements.roundCounter.innerHTML = 'Turn 1.';
-    domElements.playerTurnIndicator.innerHTML = '';
-    domElements.playerOneIcon.innerHTML = '';
-    domElements.playerTwoIcon.innerHTML = '';
-    domElements.namePlayerOne.value = '';
-    domElements.namePlayerTwo.value = '';
-    domManipulation.disableBackground(currentPlayer.background, domElements.playerOneIcon);
-    domManipulation.disableBackground(currentPlayer.background, domElements.playerTwoIcon);
-    domElements.allIcons.forEach((element) => {
-      element.addEventListener('click', choosePlayerIcon, false);
-    });
-    document.getElementById('avoid-clicks').id = '';
-    document.getElementById('avoid-clicks-p2').id = '';
+    window.localStorage.clear()
+    window.location.reload()
     domManipulation.addForm();
-  });
-  const resetIconSelect = 1;
-  saveItem('numberOfPlayer', resetIconSelect);
-  saveItem('currentPlayer', currentPlayer);
-  saveItem('count', count);
-};
+  };
 
 export default reset;
