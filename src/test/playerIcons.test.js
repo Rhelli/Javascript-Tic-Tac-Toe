7ce95ca3cc7d15614f5695c3aad5c9067c323226
Alexis <psialedev@gmail.com>
2020-11-04 12:07:46 -0600

    deleted unused imports

--- src/test/playerIcons.test.js
+++ src/test/playerIcons.test.js
@@ -1,5 +1,4 @@
 import choosePlayerIcon from '../js/modules/playerIcons';
-import { Icons } from '../js/modules/playerIcons';
 import ImageMock from './imageMock';
 
 const icon1 = new ImageMock('assets/got/cersei.png');
@@ -7,4 +6,3 @@ const icon1 = new ImageMock('assets/got/cersei.png');
 test('Returns an array', () => {
   expect(choosePlayerIcon(icon1)).toEqual(['assets/got/cersei.png']);
 });
-
--- src/test/reset.test.js
+++ src/test/reset.test.js
@@ -1,25 +1,25 @@
-import reset from '../js/modules/reset'
+import reset from '../js/modules/reset';
 
 describe('test window location\'s reload function', () => {
-    const { reload } = window.location;
-  
-    beforeAll(() => {
-      Object.defineProperty(window.location, 'reload', {
-        configurable: true,
-      });
-      window.location.reload = jest.fn();
+  const { reload } = window.location;
+
+  beforeAll(() => {
+    Object.defineProperty(window.location, 'reload', {
+      configurable: true,
     });
-  
-    afterAll(() => {
-      window.location.reload = reload;
-    });
-  
-    it('mocks reload function', () => {
-      expect(jest.isMockFunction(window.location.reload)).toBe(true);
-    });
-  
-    it('calls reload function', () => {
-      reloadFn(); // as defined above..
-      expect(window.location.reload).toHaveBeenCalled();
-    });
+    window.location.reload = jest.fn();
   });
+
+  afterAll(() => {
+    window.location.reload = reload;
+  });
+
+  it('mocks reload function', () => {
+    expect(jest.isMockFunction(window.location.reload)).toBe(true);
+  });
+
+  it('calls reload function', () => {
+    reloadFn(); // as defined above..
+    expect(window.location.reload).toHaveBeenCalled();
+  });
+});
