b5c869947e8dd474bb5b73c76d334cfd80763db0
Rory Hellier <rory.hellier@googlemail.com>
2020-11-04 20:41:42 +0000

    Created new tests for gameInit. Added initial test to validate the function as a whole. WIP

--- src/js/modules/game.js
+++ src/js/modules/game.js
@@ -1,11 +1,13 @@
 import gameboard from './gameboard';
-import playerIcons from './playerIcons';
+import playerIcons, { Icons } from './playerIcons';
 import gameInit from './gameInit';
 import * as domElements from './domElements';
 import reset from './reset';
 
 const game = () => {
-  domElements.initButton.addEventListener('click', gameInit);
+  domElements.initButton.addEventListener('click', () => {
+    gameInit(domElements.namePlayerOne, domElements.namePlayerTwo, Icons);
+  });
   domElements.resetButton.addEventListener('click', reset);
   domElements.allIcons.forEach((element) => {
     element.addEventListener('click', (event) => {
--- src/js/modules/gameInit.js
+++ src/js/modules/gameInit.js
@@ -1,29 +1,23 @@
 import * as domElements from './domElements';
-import { Icons } from './playerIcons';
 import * as domManipulation from './domManipulation';
 import player from './player';
 import { saveItem } from './localStorage';
 
-const gameInit = () => {
-  if (domElements.namePlayerOne.value === '' || domElements.namePlayerTwo.value === '') {
-    alert("Please add both players' names to continue.");
+const gameInit = (namePlayerOne, namePlayerTwo, Icons) => {
+  if (namePlayerOne.value === '' || namePlayerTwo.value === '') {
+    window.alert("Please add both players' names to continue.");
     return false;
   }
   if (Icons.length < 2) {
-    alert('Please select a character for both players to continue.');
+    window.alert('Please select a character for both players to continue.');
     return false;
   }
-  if (domElements.namePlayerOne.value !== '' && domElements.namePlayerTwo.value !== '' && Icons.length === 2) {
-    const playerOne = player(domElements.namePlayerOne.value, 'X', 1, Icons[0], 'rgba(144, 164, 174, 0.8');
-    const playerTwo = player(domElements.namePlayerTwo.value, 'O', 2, Icons[1], 'rgba(241, 196, 15, 0.8');
+  if (namePlayerOne.value !== '' && namePlayerTwo.value !== '' && Icons.length === 2) {
+    const playerOne = player(namePlayerOne.value, 'X', 1, Icons[0], 'rgba(144, 164, 174, 0.8');
+    const playerTwo = player(namePlayerTwo.value, 'O', 2, Icons[1], 'rgba(241, 196, 15, 0.8');
     const currentPlayer = playerOne;
     const oppositePlayer = playerTwo;
     const count = 1;
-    if (!playerOne.img && !playerTwo.img) {
-      alert('Please select a character for both players to continue.');
-      return false;
-    }
-
     domManipulation.displayIcon(playerOne.img, domElements.playerOneIcon);
     domManipulation.paintBackground(playerOne.background, domElements.playerOneIcon);
     domManipulation.displayIcon(playerTwo.img, domElements.playerTwoIcon);
