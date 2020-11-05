01da34e138f9834936dd5241cf7f67e434fd79d7
Rory Hellier <rory.hellier@googlemail.com>
2020-11-05 22:13:03 +0000

    Started working on domManipulation tests. Added up tp displayRounds and handling dom elements

---
+++ src/test/domManipulation.test.js
@@ -0,0 +1,42 @@
+import * as domManipulation from '../js/modules/domManipulation';
+import { roundCounter, playerTurnIndicator } from '../js/modules/domElements';
+import player from '../js/modules/player';
+
+jest.mock('../js/modules/domElements', () => ({
+  roundCounter: jest.fn(),
+  playerTurnIndicator: jest.fn(),
+  oppositePlayer: jest.fn(),
+}));
+
+
+const container = document.createElement('div');
+
+it('removes a form from the container', () => {
+  container.id = 'form-container';
+  container.style.display = 'flex';
+  domManipulation.removeForm(container);
+  expect(container.style.display).toBe('none');
+});
+
+it('displays a freaking icon', () => {
+  const icon = 'icon.something';
+  domManipulation.displayIcon(icon, container);
+  expect(container).toBeTruthy();
+})
+
+it('paints backgrounds of icons', () => {
+  domManipulation.paintBackground('#404040', container);
+  expect(container).toHaveProperty("style.backgroundColor", 'rgb(64, 64, 64)');
+})
+
+it('sets the background of an item to its initial transparent value', () => {
+  domManipulation.initialBackground(container);
+  expect(container).toHaveProperty("style.backgroundColor", 'transparent');
+});
+
+it('displays the round of the game', () => {
+  roundCounter.mockImplementation(() => 'No Turns Yet');
+  const oppositePlayer = player("fun", "X", 1, "imagepath", "backgroundColor");
+  domManipulation.displayRounds();
+  expect(roundCounter).toBe('Turn 1');
+})
