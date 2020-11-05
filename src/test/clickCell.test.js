fd7a198decdf0032b5c2ec5b5d7f77dfaeb74b7b
Alexis <psialedev@gmail.com>
2020-11-05 12:02:48 -0600

    working on the clickCell test, currently mocked an event object

--- src/test/clickCell.test.js
+++ src/test/clickCell.test.js
@@ -1,16 +1,32 @@
-import clickCell from '../js/modules/clickCell';
-import player from '../js/modules/player';
+import clickCell from "../js/modules/clickCell";
+import player from "../js/modules/player";
 
-const playerOne = player('fun', 'X', 1, 'imagepath', 'backgroundColor');
-const playerTwo = player('fan', 'X', 1, 'imagepath', 'backgroundColor');
+const playerOne = player("fun", "X", 1, "imagepath", "backgroundColor");
+const playerTwo = player("fan", "X", 1, "imagepath", "backgroundColor");
 const namePlayerOne = playerOne.name;
 const namePlayerTwo = playerTwo.name;
-const emptyPlayer = '';
-const event = document.createElement('div');
+const emptyPlayer = "";
+const event = document.createElement("div");
+
+const mockEvent = {
+  target: {
+    appendChild(imageElement) {
+        console.log(imageElement);
+      },
+      classList: {
+        add: [],
+      },
+    dataset: {
+      datasymbol: "",
+    },
+  },
+};
 
 document.body.append(event);
 window.alert = jest.fn();
 
-it('passes', () => {
-  expect(clickCell(event, namePlayerOne, namePlayerTwo, playerOne)).toBeTruthy();
+it("passes", () => {
+  expect(
+    clickCell(mockEvent, namePlayerOne, namePlayerTwo, playerOne)
+  ).toBeTruthy();
 });
