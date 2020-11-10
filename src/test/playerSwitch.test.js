084b731e5a1da9c6cd08aa9091411e18912fd2b9
Rory Hellier <rory.hellier@googlemail.com>
2020-11-10 20:37:26 +0000

    Modified playerSwitch test trying out different mocking implementations of the updateBoardArray function.

--- src/test/playerSwitch.test.js
+++ src/test/playerSwitch.test.js
@@ -1,3 +1,5 @@
+import updateBoardArray from '../js/modules/updateBoard';
+
 const { expect } = require('@jest/globals');
 const { default: player } = require('../js/modules/player');
 const { default: playerSwitch } = require('../js/modules/playerSwitch');
@@ -13,23 +15,29 @@ jest.mock('../js/modules/domElements', () => ({
   oppositePlayer: jest.fn(),
 }));
 
-const cells = [];
-jest.mock('../js/modules/updateBoard', () => ({
-  updateBoardArray: jest.fn(cells),
-}));
+//jest.mock('../js/modules/updateBoard.js', () => ({
+//  updateBoardArray: jest.fn().mockImplementation(() => true),
+//}));
+
+//jest.mock('../js/modules/updateBoard', () => ({
+//  default: () => ({
+//    updateBoardArray: jest.fn()
+//  }),
+//}));
 
 test("if there's no winner and the game hasn't ended it switchs current player and opposite player", () => {
   document.body.innerHTML = '<h3 id="turn-counter">No Turns Yet</h3>';
-
+  const cells = 'div.#c-3 data-datasymbol="lol"';
   const playerOne = player('Alex', 'X', 1, 'O', 'rgba(144, 164, 174, 0.8');
   const playerTwo = player('Rory', 'O', 2, 'x', 'rgba(241, 196, 15, 0.8');
   const currentPlayer = playerOne;
   const oppositePlayer = playerTwo;
   const count = 0;
-
   playerSwitch(currentPlayer,
     oppositePlayer, playerOne,
     playerTwo,
-    count);
+    count,
+    updateBoardArray,
+    cells);
   expect(currentPlayer).toEqual(playerTwo);
 });
